5db9bd12cd4f32763b8b76b7bd773793d6382611
samueldesmit <samuel_desmit@hotmail.com>
2023-01-19 16:38:06 +0100

    loops + bonus af

--- loops.js
+++ loops.js
@@ -6,7 +6,9 @@
 // Hoera!
 // ==========================================
 
-
+for (let i = 0; i < 3; i++) {
+    console.log("Hoera!");
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -16,9 +18,15 @@
 // loop...
 // loop...
 // klaar!
- // ==========================================
+// ==========================================
 
-
+for (let i = 0; i < 5; i++) {
+    if (i < 4) {
+        console.log("Loop...");
+    } else {
+        console.log("Klaar!");
+    }
+}
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -33,7 +41,13 @@
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
 
-
+for (let i = 0; i < 16; i++) {
+    if ((i % 2) < 1) {
+        console.log(i + " is even");
+    } else {
+        console.log(i + " is oneven");
+    }
+}
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -50,4 +64,16 @@
 // >> 8
 // >> 9
 // ==========================================
+
+for (let i = 0; i < 10; i++) {
+    if (i < 3 ){
+        console.log ("" + i)
+    }
+    else if (i < 6) {
+        console.log ("> " + i)
+    }
+    else {
+        console.log (">> " + i)
+    }
+}
 
