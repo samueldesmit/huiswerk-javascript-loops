9e72e8e1e853f68ca20f0f5d221d2a2dcfe047c6
samueldesmit <samuel_desmit@hotmail.com>
2023-01-19 16:58:19 +0100

    loops and arrays af

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -13,6 +13,10 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
 
+for (let i = 0; i < names.length; i++) {
+    // console.log (names[i]+"je")
+
+}
 
 
 // ==========================================
@@ -23,13 +27,20 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 
 const numbers = [2, 4, 5, 29, 38];
 
+for (let i = 0; i < numbers.length; ++i) {
+    // console.log (numbers[i])
+    if ((numbers[i] % 2) < 1) {
+        console.log(numbers[i] * 2)
+    } else {
+        console.log(numbers[i] * 3)
+    }
+}
+
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(numbers) geeft: [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 4, 8, 15, 87, 76 ];
-
-
 
 
 // ==========================================
@@ -41,6 +52,10 @@ const numbers = [2, 4, 5, 29, 38];
 // ==========================================
 
 const squares = [30, 2, 8, 24, 11];
+
+for (let i = 0; i < squares.length; i++) {
+    console.log("het volume van " + squares[i] + " is " + squares[i] * squares[i] * squares[i])
+}
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
